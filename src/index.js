8aa66a00c91e491bc0d580027cce699fd60c68ed
Arupyaa <doudiat1998@gmail.com>
2025-06-20 21:52:20 +0300

    tested project sorting methods

--- src/index.js
+++ src/index.js
@@ -4,15 +4,32 @@ import { TodosHandler } from './todosHandler';
 
 let project = new Project();
 TodosHandler.addTodo(project,"do HW","do my homework",new Date("2025-06-21T00:00"));
-TodosHandler.addTodo(project,"Tidy room","tidy your room",new Date("2025-06-21T12:30"),"don't procrascinate",{
+TodosHandler.addTodo(project,"Tidy room","tidy your room",new Date("2025-06-19T12:30"),"don't procrascinate",{
     bedroom:false,
     desk:false,
     closet:false
 });
 TodosHandler.addTodo(project,"sleep","go to bed",new Date("2025-06-23T03:10"),"nonsense");
 
-TodosHandler.removeTodo(project,project.list[1].id);
+
+
+console.log("original order:");
+console.log(JSON.parse(JSON.stringify(project)));
+
+project.sortByPriority("DESC");
+console.log("reversed order:");
+console.log(JSON.parse(JSON.stringify(project)));
+
+project.sortByDueDate("ASC");
+console.log("ascending dueDate:");
+console.log(JSON.parse(JSON.stringify(project)));
+
+project.sortByDueDate("DESC");
+console.log("descending dueDate:");
+console.log(JSON.parse(JSON.stringify(project)));
+
+/* TodosHandler.removeTodo(project,project.list[1].id);
 console.log(JSON.parse(JSON.stringify(project)));
 
 TodosHandler.editChecklist(project,project.list[0].id,{english:false,french:false});
-console.log(JSON.parse(JSON.stringify(project)));
+console.log(JSON.parse(JSON.stringify(project))); */
