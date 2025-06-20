bd3e416280bff37611e70bf38ced5aa771a8ba6e
Arupyaa <doudiat1998@gmail.com>
2025-06-21 01:41:10 +0300

    testing display todos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import './style.css'
+import './todos.css'
 import { Project } from "./projects.js";
 import { TodosHandler } from './todosHandler';
 import { TodosInterface } from './interface.js';
 
+let projectContainer = document.createElement("div");
+projectContainer.classList.add("project-container");
+let mainContainer = document.querySelector(".container");
+mainContainer.appendChild(projectContainer);
+
 let project = new Project();
 TodosHandler.addTodo(project,"do HW","do my homework",new Date("2025-06-21T00:00"));
-TodosHandler.addTodo(project,"Tidy room","tidy your room",new Date("2025-06-19T12:30"),"don't procrascinate",{
-    bedroom:false,
-    desk:false,
-    closet:false
-});
+TodosHandler.addTodo(project,"Tidy room","tidy your room",new Date("2025-06-19T12:30"),"don't procrascinate",[
+    {name:"bedroom",state:false},
+    {name:"desk",state:true},
+    {name:"closet",state:false}
+]
+);
 TodosHandler.addTodo(project,"sleep","go to bed",new Date("2025-06-23T03:10"),"nonsense");
 
+TodosInterface.displayTodos(projectContainer,project);
 
-
-console.log("original order:");
+/* console.log("original order:");
 console.log(JSON.parse(JSON.stringify(project)));
 
 project.sortByPriority("DESC");
@@ -27,7 +34,7 @@ console.log(JSON.parse(JSON.stringify(project)));
 
 project.sortByDueDate("DESC");
 console.log("descending dueDate:");
-console.log(JSON.parse(JSON.stringify(project)));
+console.log(JSON.parse(JSON.stringify(project))); */
 
 /* TodosHandler.removeTodo(project,project.list[1].id);
 console.log(JSON.parse(JSON.stringify(project)));
