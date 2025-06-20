8b434c2a6febf1a0e10607fde826b5f77936b5f7
Arupyaa <doudiat1998@gmail.com>
2025-06-20 16:13:40 +0300

    changed todohandler from class to module imported it into index.js and with Project class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './style.css'
+import { Project } from "./projects.js";
+import { TodosHandler } from './todosHandler';
--- src/todosHandler.js
+++ src/todosHandler.js
@@ -1,13 +1,14 @@
 import { Todos } from "./todos";
-import { Project } from "./projects";
+export { TodosHandler }
 
-class TodosHandler {
-    addTodo(project, title, description, dueDate, notes = "", checklist = []) {
+let TodosHandler = (function () {
+
+    let addTodo = function (project, title, description, dueDate, notes = "", checklist = []) {
         let todo = new Todos(crypto.randomUUID(), title, description, dueDate, notes, checklist, project.list.length + 1);
         project.list.push(todo);
     }
 
-    removeTodo(project, id) {
+    let removeTodo = function (project, id) {
         let index = project.list.findIndex((todo) => todo.id == id);
         if (index != -1) {
             let removed = project.list.splice(index, 1);
@@ -17,7 +18,7 @@ class TodosHandler {
             return false;
     }
 
-    increasePriority(project, id) {
+    let increasePriority = function (project, id) {
         let index = project.list.findIndex((todo) => todo.id == id);
         if (index != -1 && index != 0) {
             let t = project.list[index];
@@ -26,9 +27,9 @@ class TodosHandler {
         }
     }
 
-    decreasePriority(project, id) {
+    let decreasePriority = function (project, id) {
         let index = project.list.findIndex((todo) => todo.id == id);
-        if (index != -1 && index != project.list.length-1) {
+        if (index != -1 && index != project.list.length - 1) {
             let t = project.list[index];
             project.list[index] = project.list[index + 1];
             project.list[index + 1] = t;
@@ -36,12 +37,14 @@ class TodosHandler {
     }
 
     //proper input validation is handled by the DOM module
-    editTitle = makeAnEdit(title);
-    editDescription = makeAnEdit(description);
-    editDueDate = makeAnEdit(dueDate);
-    editNotes = makeAnEdit(notes);
-    editChecklist = makeAnEdit(checklist);
-}
+    let editTitle = makeAnEdit(title);
+    let editDescription = makeAnEdit(description);
+    let editDueDate = makeAnEdit(dueDate);
+    let editNotes = makeAnEdit(notes);
+    let editChecklist = makeAnEdit(checklist);
+
+    return { addTodo, removeTodo, increasePriority, decreasePriority, editTitle, editDescription, editDueDate, editNotes, editChecklist };
+})();
 
 let makeAnEdit = function (editField) {
     let edit = function (project, id, value) {
