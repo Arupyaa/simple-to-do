79422f3205dd318fa702985adf490f333372bb41
Arupyaa <doudiat1998@gmail.com>
2025-06-26 16:36:29 +0300

    changed logic for undefined todo fields and added delete icon svg to some fields in interface module

--- src/interface.js
+++ src/interface.js
@@ -3,9 +3,14 @@ import { modal } from "./modal";
 export { TodosInterface };
 
 let TodosInterface = (function () {
-    let makeSVG = createSVG("svg", "edit-icon", { viewBox: "0 0 1024 1024" });
+    let makeEditSVG = createSVG("svg", "edit-icon", { viewBox: "0 0 1024 1024" });
     let makePath1 = createSVG("path", "edit-path", { d: "M81.23 959.96c-4.51 0-8.91-1.78-12.18-5.05a17.2 17.2 0 0 1-4.44-16.7c7.78-28.66 77.36-281.32 123.73-327.69l508.14-508.14c51.15-51.12 134.31-51.09 185.4 0l39.73 39.71c24.75 24.76 38.4 57.68 38.4 92.7s-13.65 67.93-38.4 92.67l-508.2 508.21c-46.35 46.34-299 115.91-327.66 123.69-1.5 0.4-3.01 0.6-4.52 0.6zM789.19 98.48c-24.75 0-49.5 9.41-68.35 28.26L212.7 634.88c-27.65 27.66-76.01 174.78-106.71 283.09 108.31-30.7 255.41-79.04 283.05-106.66L897.23 303.1c18.24-18.24 28.3-42.5 28.3-68.31 0-25.81-10.06-50.08-28.3-68.33l-39.73-39.71c-18.82-18.84-43.56-28.27-68.31-28.27z m-387.97 725h0.17-0.17z" });
     let makePath2 = createSVG("path", "edit-path", { d: "M856.04 385.83c-4.41 0-8.82-1.68-12.18-5.05L643.13 180.03c-6.73-6.73-6.73-17.64 0-24.37s17.63-6.73 24.37 0l200.73 200.76c6.73 6.73 6.73 17.64 0 24.37a17.211 17.211 0 0 1-12.19 5.04zM405.9 836.02c-4.41 0-8.82-1.68-12.18-5.05L192.96 630.23c-6.73-6.73-6.73-17.63 0-24.37s17.63-6.73 24.37 0L418.09 806.6c6.73 6.73 6.73 17.63 0 24.37a17.18 17.18 0 0 1-12.19 5.05z" });
+    let makeDeleteSVG = createSVG("svg", "delete-icon", { viewBox: "0 0 24 24" });
+    let makePath3 = createSVG("path", "edit-path", {
+        d: "M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z",
+        "fill-rule": "evenodd", "clip-rule": "evenodd"
+    });
 
     let displayTodos = function (container, project) {
         project.list.forEach(todo => {
@@ -18,7 +23,7 @@ let TodosInterface = (function () {
             let title = document.createElement("h3");
             title.classList.add("title");
             title.textContent = todo.title;
-            let titleEdit = makeSVG();
+            let titleEdit = makeEditSVG();
             titleEdit.appendChild(makePath1());
             titleEdit.appendChild(makePath2());
             titleEdit.addEventListener("click", function () {
@@ -32,7 +37,7 @@ let TodosInterface = (function () {
             let dueDate = document.createElement("div");
             dueDate.textContent = dateFormat(todo.dueDate, "MMM d h:mm aa");
             dueDate.classList.add("date");
-            let dueDateEdit = makeSVG();
+            let dueDateEdit = makeEditSVG();
             dueDateEdit.appendChild(makePath1());
             dueDateEdit.appendChild(makePath2());
             dueDateEdit.addEventListener("click", function () {
@@ -46,34 +51,40 @@ let TodosInterface = (function () {
             let description = document.createElement("p");
             description.classList.add("description");
             description.textContent = todo.description;
-            let descriptionEdit = makeSVG();
+            let descriptionEdit = makeEditSVG();
             descriptionEdit.appendChild(makePath1());
             descriptionEdit.appendChild(makePath2());
             descriptionEdit.addEventListener("click", function () {
                 modal.editDescription(project, this.parentNode.parentNode.parentNode.dataset.id);
             });
+            let descriptionDelete = makeDeleteSVG();
+            descriptionDelete.appendChild(makePath3());
             let descriptionTitle = document.createElement("h4");
             descriptionTitle.textContent = "Description:";
             let descriptionBox = document.createElement("div");
             descriptionBox.classList.add("description-box");
             descriptionBox.appendChild(descriptionTitle);
+            descriptionBox.appendChild(descriptionDelete);
             descriptionBox.appendChild(descriptionEdit);
             descriptionBox.appendChild(description);
 
             let notes = document.createElement("p");
             notes.classList.add("notes");
             notes.textContent = todo.notes;
-            let notesEdit = makeSVG();
+            let notesEdit = makeEditSVG();
             notesEdit.appendChild(makePath1());
             notesEdit.appendChild(makePath2());
             notesEdit.addEventListener("click", function () {
                 modal.editNotes(project, this.parentNode.parentNode.parentNode.dataset.id);
             });
+            let notesDelete = makeDeleteSVG();
+            notesDelete.appendChild(makePath3());
             let notesTitle = document.createElement("h4");
             notesTitle.textContent = "Notes:";
             let notesBox = document.createElement("div");
             notesBox.classList.add("notes-box");
             notesBox.appendChild(notesTitle);
+            notesBox.appendChild(notesDelete);
             notesBox.appendChild(notesEdit);
             notesBox.appendChild(notes);
 
@@ -94,29 +105,37 @@ let TodosInterface = (function () {
                     checklist.appendChild(list);
                 })
             }
-            let checklistEdit = makeSVG();
+            let checklistEdit = makeEditSVG();
             checklistEdit.appendChild(makePath1());
             checklistEdit.appendChild(makePath2());
             checklistEdit.addEventListener("click", function () {
                 modal.editChecklist(project, this.parentNode.parentNode.parentNode.dataset.id);
             });
+            let checklistDelete = makeDeleteSVG();
+            checklistDelete.appendChild(makePath3());
             let checklistTitle = document.createElement("h4");
             checklistTitle.textContent = "Checklist:";
             let checklistBox = document.createElement("div");
             checklistBox.classList.add("checklist-box");
             checklistBox.appendChild(checklistTitle);
+            checklistBox.appendChild(checklistDelete);
             checklistBox.appendChild(checklistEdit);
             checklistBox.appendChild(checklist);
+
+            //hide a specific todo field if not defined by user
+            if (todo.description == undefined)
+                descriptionBox.setAttribute("style", "display:none");
+            if (todo.notes == undefined)
+                notesBox.setAttribute("style", "display:none");
+            if (todo.checklist == undefined)
+                checklistBox.setAttribute("style", "display:none");
 
             card.appendChild(header);
             cardBody.appendChild(titleBox);
             cardBody.appendChild(dueDateBox);
-            if (todo.description != undefined)
-                cardBody.appendChild(descriptionBox);
-            if (todo.notes != undefined)
-                cardBody.appendChild(notesBox);
-            if (todo.checklist != undefined)
-                cardBody.appendChild(checklistBox);
+            cardBody.appendChild(descriptionBox);
+            cardBody.appendChild(notesBox);
+            cardBody.appendChild(checklistBox);
             card.appendChild(cardBody);
             card.dataset.id = todo.id;
             container.appendChild(card);
